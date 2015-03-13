4815f9ec72cfd91feaffeb1c6273ec0359c68d63
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-13 17:46:39 +0000

    task_05

--- task_05.py
+++ task_05.py
@@ -3,15 +3,12 @@
 """A docstring."""
 
 to_flip = [(1, 2, 3), 'hello']
-i = 0
 def flip_keys(to_flip):
-    for i in to_flip:
-        to_flip[:i]
+    
+    """to_flip list returns a list"""
+    i = 0
+    for item in to_flip:
+        to_flip[i] = item[::-1]
         i += 1
     return to_flip
 print flip_keys(to_flip)
-        
-
-
-    
-        
