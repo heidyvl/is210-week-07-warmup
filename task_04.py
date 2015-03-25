fead4618b212fd9e96cf9f1ad296f779538be08b
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-25 22:08:39 +0000

    task_04

--- task_04.py
+++ task_04.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 """A docstring."""
 
-data = [1, 2, 3.5]
+
 def process_data(data):
     """returns addition and average"""
-    
+    avg = 0.0
     for item in data:
-        tup = (sum(data), sum(data)/len(data))
+        avg += item
+        tup = (sum(data), avg/len(data))
     return tup
-print process_data(data)
