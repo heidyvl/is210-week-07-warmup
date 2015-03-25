b58355d2b5c8ab08dc2a8ca4a8f6a916528d0553
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-25 22:06:04 +0000

    task_01

--- task_01.py
+++ task_01.py
@@ -2,5 +2,8 @@
 # -*- coding: utf-8 -*-
 """A docstring."""
 
-ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon')
-OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division', 'Addition', 'Subtraction']
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
+                    'Addition', 'Subtraction']
